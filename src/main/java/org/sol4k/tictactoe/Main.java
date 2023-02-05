2cbf55d6dc6b7e656b53fedd69dd657e03ef540b
Sasha Shpota <5640984+Shpota@users.noreply.github.com>
2023-02-05 12:56:03 +0300

    Add game trasactions (#2)

--- src/main/java/org/sol4k/tictactoe/Main.java
+++ src/main/java/org/sol4k/tictactoe/Main.java
@@ -1,7 +1,21 @@
 package org.sol4k.tictactoe;
 
+import org.sol4k.Base58;
+import org.sol4k.Connection;
+import org.sol4k.Keypair;
+import org.sol4k.PublicKey;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var programId = new PublicKey("4Y5g1y7JXb2hpHDWsGQTAhHKbSJtR95W6QGzoXgESN8U");
+        var gameAccount = new PublicKey(args[0]);
+        var connection = new Connection("https://api.devnet.solana.com");
+        var playerKeypair = Keypair.fromSecretKey(Base58.decode(
+                "2WGcYYau2gLu2DUq68SxxXQmCgi77n8hFqqLNbNyg6Xfh2m3tvg8LF5Lgh69CFDux41LUKV1ak1ERHUqiBZnyshz"
+        ));
+
+        var game = new Game(connection, programId, gameAccount, playerKeypair);
+        game.join();
+        game.play();
     }
 }
